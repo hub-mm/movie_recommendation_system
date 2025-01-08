8b2ac9cd4d454912ca7888d7f5f67568d7999197
hub-mm <165568972+hub-mm@users.noreply.github.com>
2025-01-08 23:35:18 +0000

    Create preprocessing of movies_metadata.csv

---
+++ scripts/preprocess_data.py
@@ -0,0 +1,13 @@
+# ./scripts/preprocess_data.py
+from ast import literal_eval
+import pandas as pd
+
+
+def preprocess(filepath='./data/movies_dataset/7/movies_metadata.csv'):
+    df = pd.read_csv(filepath, dtype='unicode')
+    df['release_date'] = pd.to_datetime(df['release_date'], errors='coerce').apply(lambda x: x.date())
+    df['genres'] = df['genres'].fillna('[]').apply(literal_eval).apply(
+        lambda x: [i['name'] for i in x] if isinstance(x, list) else []
+    )
+
+    return df
