507b74e04e643556b42362e9fb229c262cc09e3d
hub-mm <165568972+hub-mm@users.noreply.github.com>
2025-01-08 23:36:21 +0000

    Build script for top chart

---
+++ scripts/top_chart_data.py
@@ -0,0 +1,29 @@
+# ./scripts/top_chart_data.py
+from preprocess_data import preprocess
+
+
+def top_chart(top_chart_num=250):
+    df = preprocess()
+
+    vote_counts = df[df['vote_count'].notnull()]['vote_count'].astype('float')
+    vote_av = df[df['vote_average'].notnull()]['vote_average'].astype('float')
+    vote_av_mean = vote_av.mean()
+    min_votes = vote_counts.quantile(0.95)
+
+    qualified = df[
+        (df['vote_count'].astype('float') >= min_votes) & (df['vote_count'].notnull()) & (df['vote_average'].notnull())
+    ][
+        ['title', 'release_date', 'vote_count', 'vote_average', 'popularity', 'genres']
+    ]
+    qualified['vote_count'] = qualified['vote_count'].astype('float')
+    qualified['vote_average'] = qualified['vote_average'].astype('float')
+
+    qualified['wr'] = qualified.apply(
+        lambda x: (
+        x['vote_count'] / (x['vote_count'] + min_votes) * x['vote_average']) + (min_votes / (min_votes + x['vote_count']) * vote_av_mean),
+        axis=1
+    )
+
+    qualified = qualified.sort_values('wr', ascending=False).head(top_chart_num)
+
+    return qualified
