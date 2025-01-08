6073a2a1eac24ad1ff902bb0b5ea381177718537
hub-mm <165568972+hub-mm@users.noreply.github.com>
2025-01-08 23:33:49 +0000

    Edit layout

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -1,8 +1,9 @@
 # ./scripts/download_data.py
-import os
-import shutil
 import logging
+import os
 import kagglehub
+import shutil
+
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -16,6 +17,7 @@ def download_movie_data(dataset_path='./data/movies_dataset'):
         # Download from Kaggle: The Movie Dataset Path
         default_path = kagglehub.dataset_download("rounakbanik/the-movies-dataset")
         shutil.move(default_path, dataset_path)
+
     logger.info(f"Path to dataset file: {dataset_path}")
 
 download_movie_data()
