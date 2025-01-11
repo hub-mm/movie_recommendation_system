4aaffdc54c237ba9424990a9c93678a0dc873f30
hub-mm <165568972+hub-mm@users.noreply.github.com>
2025-01-11 17:25:10 +0000

    Rename for clarity and differentiation

--- scripts/data_management/data_preprocess_movies_metadata.py
+++ scripts/data_management/data_preprocess_movies_metadata.py
@@ -3,7 +3,7 @@ from ast import literal_eval
 import pandas as pd
 
 
-def preprocess(filepath='./data/movies_dataset/7/movies_metadata.csv'):
+def preprocess_movies_md(filepath='./data/movies_dataset/7/movies_metadata.csv'):
     df = pd.read_csv(filepath, dtype='unicode')
     df['release_date'] = pd.to_datetime(df['release_date'], errors='coerce').apply(lambda x: x.date())
     df['genres'] = df['genres'].fillna('[]').apply(literal_eval).apply(
