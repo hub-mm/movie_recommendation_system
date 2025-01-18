6d22bc885482a9c0ef8fff66b67c78e553e1b44b
hub-mm <165568972+hub-mm@users.noreply.github.com>
2025-01-18 22:30:37 +0000

    Web app to manage python and html interaction using Flask

---
+++ app/web_app.py
@@ -0,0 +1,33 @@
+# ./app/web_app.py
+from main import main_func
+from flask import Flask, redirect, url_for, request, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/home')
+def home():
+    movies = main_func(5)
+    return render_template('index.html', movies=movies)
+
+@app.route('/rating')
+def rating():
+    movies = main_func(1, amount=20)
+    return render_template('rating.html', movies=movies)
+
+@app.route('/genre')
+def genre():
+    genre_val = request.args.get('genre', 'action').strip()
+    movies = main_func(2, amount=20, genre=genre_val)
+
+    return render_template('genre.html', movies=movies)
+
+@app.route('/similar')
+def similar():
+    title_val = request.args.get('title', 'The Godfather').strip().lower()
+    movies = main_func(4, amount=20, title=title_val)
+
+    return render_template('similar.html', movies=movies)
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
