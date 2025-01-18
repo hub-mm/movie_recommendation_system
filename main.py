04f1b2df8ae76ccf692edbe16a402d7c9a0eb2ae
hub-mm <165568972+hub-mm@users.noreply.github.com>
2025-01-18 22:34:37 +0000

    Main file to manage scripts

--- main.py
+++ main.py
@@ -3,21 +3,18 @@ from scripts.top_chart.top_chart_rating import build_top_chart
 from scripts.top_chart.top_chart_genre import build_genre_chart
 from scripts.top_chart.top_chart_similar import build_similar_chart
 from scripts.top_chart.top_chart_hybrid import build_chart_hybrid
+from scripts.script_app.random_image import random_img
 
 
-def main(choice, amount=10, genre='action', title='Batman', user_id=1):
+def main_func(choice, amount=10, genre='action', title='Batman', user_id=1):
     match int(choice):
         case 1:
-            print(build_top_chart().head(amount))
+            return build_top_chart(amount=amount)
         case 2:
-            print(build_genre_chart(genre).head(amount))
+            return build_genre_chart(genre=genre, amount=amount)
         case 3:
             print(build_similar_chart(title, amount))
         case 4:
-            print(build_chart_hybrid(user_id, title, amount))
-
-if __name__ == '__main__':
-    # main(1)
-    # main(2, amount=10, genre='horror')
-    # main(3, amount=10, title='happy feet')
-    main(4, amount=10, title='mean girls', user_id=5000)
+            return build_chart_hybrid(user_id, title=title, amount=amount)
+        case 5:
+            return random_img()
